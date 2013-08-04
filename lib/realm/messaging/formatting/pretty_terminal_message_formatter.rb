fbeb7cd9935c417186796423ffae548f597a5524
Ash Moran <ash.moran@patchspace.co.uk>
2013-08-04 19:28:40 +0100

    Do care about race conditions and weird threading behaviour after all

--- lib/realm/messaging/formatting/pretty_terminal_message_formatter.rb
+++ lib/realm/messaging/formatting/pretty_terminal_message_formatter.rb
@@ -6,38 +6,48 @@ module Realm
       class PrettyTerminalMessageFormatter
         include Term::ANSIColor
 
-        class << self
-          def inspector
-            # Don't care about race conditions
-            @inspector ||=
-              AwesomePrint::Inspector.new(
-                multiline: false,
-                sort_keys: true
-              )
+        # Because of the way AwesomePrint manages thread concurrency,
+        # we have to have something we can initialize for each format run
+        class PrettyAwesomeFormatter
+          include Term::ANSIColor
+
+          def initialize(inspector)
+            @inspector = inspector
+          end
+
+          def attributes_string(attributes)
+            attributes.map { |property, attribute|
+              magenta(property.to_s) << ": " << @inspector.awesome(attribute)
+            }.join(", ")
           end
         end
 
         def format(attrs)
-          # inspector.awesome(message_attributes)
+          attributes_string =
+            new_attributes_formatter.attributes_string(
+              attrs.fetch(:attributes)
+            )
+
           "<" <<
             cyan(attrs.fetch(:category).to_s) << " " <<
             "type=" << magenta(attrs.fetch(:type).to_s) << " " <<
             "version=" << attrs.fetch(:version).to_s << " " <<
             "timestamp=" << attrs.fetch(:timestamp).inspect << " " <<
-            "attributes={" << attributes_string(attrs.fetch(:attributes)) << "}" <<
+            "attributes={" << attributes_string << "}" <<
           ">"
         end
 
         private
 
-        def attributes_string(attributes)
-          attributes.map { |property, attribute|
-            magenta(property.to_s) << ": " << inspector.awesome(attribute)
-          }.join(", ")
+        def new_attributes_formatter
+          PrettyAwesomeFormatter.new(new_inspector)
         end
 
-        def inspector
-          self.class.inspector
+        def new_inspector
+          AwesomePrint::Inspector.new(
+            multiline: false,
+            sort_keys: true
+          )
         end
       end
     end
